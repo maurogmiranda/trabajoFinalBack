a36702c3b389fc9fff9faea2703e2fe294db85b1
maurogmiranda185 <maurogmiranda185@gmail.com>
2022-04-21 21:43:59 -0300

    Oauth2

--- gateway-service/src/main/java/com/example/gatewayservice/configuration/OIDCTokenRelayConfiguration.java
+++ gateway-service/src/main/java/com/example/gatewayservice/configuration/OIDCTokenRelayConfiguration.java
@@ -1,22 +1,22 @@
-//package com.example.gatewayservice.configuration;
-//
-//import com.example.gatewayservice.filter.OIDCTokenRelay;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.boot.autoconfigure.security.SecurityProperties;
-//import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledFilter;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//@Configuration(proxyBeanMethods = false)
-//@ConditionalOnProperty(name = "spring.cloud.gateway.enabled", matchIfMissing = true)
-//@ConditionalOnClass({ OAuth2AuthorizedClient.class, SecurityWebFilterChain.class, SecurityProperties.class })
-//@ConditionalOnEnabledFilter(OIDCTokenRelay.class)
-//public class OIDCTokenRelayConfiguration {
-//    @Bean
-//    public OIDCTokenRelay myOIDCTokenRelay() {
-//        return new OIDCTokenRelay();
-//    }
-//}
+package com.example.gatewayservice.configuration;
+
+import com.example.gatewayservice.filter.OIDCTokenRelay;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(name = "spring.cloud.gateway.enabled", matchIfMissing = true)
+@ConditionalOnClass({ OAuth2AuthorizedClient.class, SecurityWebFilterChain.class, SecurityProperties.class })
+@ConditionalOnEnabledFilter(OIDCTokenRelay.class)
+public class OIDCTokenRelayConfiguration {
+    @Bean
+    public OIDCTokenRelay myOIDCTokenRelay() {
+        return new OIDCTokenRelay();
+    }
+}
